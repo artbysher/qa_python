10d7f57d8afaf2bec7f813e8368d22b06b87e03b
Alexandra Sher <ya-sher-ka@yandex.ru>
2025-03-09 23:10:49 +0400

    Добавила недостающие тесты, фикстуру, параметризацию  и добавила структуру

--- tests.py
+++ tests.py
@@ -1,16 +1,19 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
     # пример теста:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+
+# Тест 1
+
+    def test_add_new_book_add_two_books(self,collector):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
 
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -24,8 +27,10 @@ class TestBooksCollector:
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
-    def test_add_new_book_add_duplicate_book(self):
-            collector = BooksCollector()
+# Тест 2
+
+    def test_add_new_book_add_duplicate_book(self, collector):
+
 
             collector.add_new_book('Гордость и предубеждение и зомби')
             count_before = len(collector.get_books_genre())
@@ -34,23 +39,33 @@ class TestBooksCollector:
 
             assert len(collector.get_books_genre()) == count_before
 
+# Тест 3
 
-    def test_set_book_genre_check_genre_detection(self):
-        collector = BooksCollector()
+    def test_set_book_genre_check_genre_detection(self, collector):
 
         collector.add_new_book("Приключения Шерлока Холмса")
         collector.set_book_genre("Приключения Шерлока Холмса", "Детективы")
         assert collector.get_book_genre("Приключения Шерлока Холмса") == "Детективы"
 
-    def test_set_book_genre_check_invalid_genre(self):
-        collector = BooksCollector()
+# Тест 4
 
-        collector.add_new_book("Хроники Нарнии")
-        collector.set_book_genre("Хроники Нарнии", "Фентези")
-        assert collector.get_book_genre("Хроники Нарнии") == ""
+    @pytest.mark.parametrize(
+        'name, invalid_genre',
+        [
+        ('Хроники Нарнии', 'Кино'),
+        ('Хроники Нарнии', 'Музыка'),
+        ('Хроники Нарнии', 'Фэнтези'),
+        ('Хроники Нарнии', 'Аниме'),
+    ])
+    def test_set_book_genre_check_invalid_genre(self, collector, name, invalid_genre):
+        collector.add_new_book(name)
+        assert collector.get_book_genre(name) == ""
+        collector.set_book_genre(name, invalid_genre)
+        assert collector.get_book_genre(name) == ""
 
-    def test_get_books_with_specific_genre_simple_genres(self):
-        collector = BooksCollector()
+# Тест 5
+
+    def test_get_books_with_specific_genre_simple_genres(self, collector):
 
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
@@ -60,23 +75,24 @@ class TestBooksCollector:
 
         assert collector.get_books_with_specific_genre('Ужасы') == ['Что делать, если ваш кот хочет вас убить']
 
-    def test_get_books_genre_empty(self):
-        collector = BooksCollector()
+# Тест 6
+
+
+    def test_get_books_genre_empty_dictionary_when_no_books(self, collector):
 
         assert collector.get_books_genre() == {}
 
-    def test_get_list_of_favorites_books_with_books(self):
-        collector = BooksCollector()
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Хроники Нарнии')
+# Тест 7
 
-        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
-        collector.add_book_in_favorites('Хроники Нарнии')
+    def test_get_book_genre_returns_empty_for_unset_genre(self, collector):
 
-        assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби', 'Хроники Нарнии']
+        collector.add_new_book('Почему на Руси жить хорошо?')
+        assert collector.get_book_genre('Почему на Руси жить хорошо?') == ""
 
-    def test_get_books_for_children_one_unsuitable_book_(self):
-        collector = BooksCollector()
+# Тест 8
+
+    def test_get_books_for_children_one_unsuitable_book_(self, collector):
+
 
         collector.add_new_book('Хроники Нарнии')
         collector.set_book_genre('Хроники Нарнии', 'Фантастика')
@@ -87,8 +103,10 @@ class TestBooksCollector:
 
         assert collector.get_books_for_children() == ['Хроники Нарнии', 'Свинка Пеппа']
 
-    def test_add_book_in_favorites_duplicate_in_favorites(self):
-        collector = BooksCollector()
+# Тест 9
+
+    def test_add_book_in_favorites_duplicate_in_favorites(self, collector):
+
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
@@ -99,8 +117,10 @@ class TestBooksCollector:
 
         assert len(collector.get_list_of_favorites_books()) == count_before
 
-    def test_delete_book_from_favorites(self):
-        collector = BooksCollector()
+# Тест 10
+
+    def test_delete_book_from_favorites(self, collector):
+
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
@@ -109,3 +129,21 @@ class TestBooksCollector:
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
 
         assert 'Гордость и предубеждение и зомби' not in collector.get_list_of_favorites_books()
+
+# Тест 11
+
+    def test_get_list_of_favorites_books_with_books(self, collector):
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Хроники Нарнии')
+
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Хроники Нарнии')
+
+        assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби', 'Хроники Нарнии']
+
+# Тест 12
+
+    def test_get_list_of_favorites_books_returns_empty_list_when_no_books(self, collector):
+
+        assert collector.get_list_of_favorites_books() == []
